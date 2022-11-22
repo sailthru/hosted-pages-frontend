a5720ec832d68214b71ed04531f56ab9c6917afd
Shannon Stith <shanthru@users.noreply.github.com>
2022-11-22 15:57:14 -0500

    [EPT-1709_ADD_LIST_VIEW_AND_FETCH_PAGES] Add ListView and fetch pages

---
+++ src/ListView/ListView.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+
+import * as pagesApi from "../core/api";
+
+function ListView() {
+    const [loading, setLoading] = useState(true); // load from the start to prevent blip
+    const [pageMap, setPageMap] = useState({});
+    useEffect(getPages, []);
+
+    function getPages() {
+        pagesApi
+            .fetchPages()
+            .then((res) => {
+                const map = res.reduce((accum, curr) => {
+                    const pageId = curr?.id;
+                    if (pageId != null) {
+                        accum[pageId] = {
+                            pageId: pageId,
+                            mode: curr.mode,
+                            name: curr.name,
+                            type: curr.type,
+                            url: curr.url,
+                            modifyUser: curr.modify_user,
+                            modifyTime: curr.modify_time,
+                        };
+                    }
+                    return accum;
+                }, {});
+                setPageMap(map);
+            })
+            .catch(console.error)
+            .finally(() => setLoading(false));
+    }
+
+    function getPageById(pageId) {
+        return pageMap[pageId];
+    }
+
+    return (
+        <div>
+            {loading ? "Loading..." : <h1>Table goes here</h1>}
+        </div>
+    );
+}
+
+export { ListView };
+
+
