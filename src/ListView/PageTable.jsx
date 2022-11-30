3bd1bfbafdc6fd67c4a2af6211674b194d71bd4e
Shannon Stith <shanthru@users.noreply.github.com>
2022-11-30 10:46:34 -0500

    [EPT-1709_SEPARATE_ACTIONS_TO_NEW_FILE] Wrap in useMemo

--- src/ListView/PageTable.jsx
+++ src/ListView/PageTable.jsx
@@ -6,58 +6,58 @@ import PageTableIcons from "./PageTableIcons";
 
 
 /** Configures STUI Table to render data in columns */
-function PageTable({ loading, pages, getPageById, displayModal }) {
-  const columns = [
-    {
-      id: "page-mode-icons",
-      accessor: (p) => p,
-      width: 20,
-      disableSortBy: true,
-      Cell: (cell) => (<PageTableIcons page={cell.value} />)
-    },
-    {
-      Header: "Page Name",
-      accessor: (p) => p,
-      Cell: (cell) => (<PageTableNames page={cell.value} />)
-    },
-    {
-      Header: "URL",
-      accessor: "url",
-      disableSortBy: true,
-      width: 200,
-      Cell: function ({ cell }) {
-        return (
-          <div>
-            {" "}
-            {cell.value.url_display}{" "}
-            <a target="_blank" href={cell.value.url}>
-              <i className="fas fa-external-link-alt" title="Live Page" />
-            </a>
-          </div>
-        );
-      },
-    },
-    {
-      Header: "Category",
-      accessor: (row) => row.type[0].toUpperCase() + row.type.slice(1),
-    },
-    {
-      Header: "Last Modified",
-      accessor: "modifyTime",
-    },
-    {
-      Header: "Modify User",
-      accessor: "modifyUser",
-      disableSortBy: true,
-    },
-    {
-      id: "id",
-      accessor: (p) => p.pageId,
-      width: 0.2,
-      disableSortBy: true,
-      Cell: (cell) => (<PageTableActions pageId={cell.value} />)
-    },
-  ];
+function PageTable({ loading, pages }) {
+    const columns = React.useMemo(() => ([
+        {
+            id: "page-mode-icons",
+            accessor: (p) => p,
+            width: 20,
+            disableSortBy: true,
+            Cell: (cell) => (<PageTableIcons page={cell.value} />)
+        },
+        {
+            Header: "Page Name",
+            accessor: (p) => p,
+            Cell: (cell) => (<PageTableNames page={cell.value} />)
+        },
+        {
+            Header: "URL",
+            accessor: "url",
+            disableSortBy: true,
+            width: 200,
+            Cell: function ({ cell }) {
+                return (
+                    <div>
+                        {" "}
+                        {cell.value.url_display}{" "}
+                        <a target="_blank" href={cell.value.url}>
+                            <i className="fas fa-external-link-alt" title="Live Page" />
+                        </a>
+                    </div>
+                );
+            },
+        },
+        {
+            Header: "Category",
+            accessor: (row) => row.type[0].toUpperCase() + row.type.slice(1),
+        },
+        {
+            Header: "Last Modified",
+            accessor: "modifyTime",
+        },
+        {
+            Header: "Modify User",
+            accessor: "modifyUser",
+            disableSortBy: true,
+        },
+        {
+            id: "id",
+            accessor: (p) => p.pageId,
+            width: 0.2,
+            disableSortBy: true,
+            Cell: (cell) => (<PageTableActions pageId={cell.value} />)
+        },
+    ]), [pages]);
 
   return (
     <Table
