6380976eb4fcece4b797d755ab2961b5e70375b2
Shannon Stith <shanthru@users.noreply.github.com>
2022-12-22 15:32:15 -0500

    [EPT-1715_CREATE_PAGE_MODAL_V2] Add create page modal and form

---
+++ src/ListView/features/CreatePage/CreatePageModal.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+import { Modal, Notification } from "@sailthru/stui-components";
+import * as pagesApi from "../../../core/pagesApi";
+import { CreatePageForm } from "./CreatePageForm";
+
+function CreatePageModal({ title, mode, setDisplayModal }) {
+    const [state, setState] = useState({});
+    const [error, setError] = useState(false);
+
+    function handleCreate() {
+        pagesApi
+            .createPage({ ...state, mode })
+            .then(function (response) {
+                console.log("RESPONSE", response);
+            })
+            .catch(function (serverError) {
+                alert(JSON.stringify(serverError));
+                console.log("ERROR:", serverError);
+                setError(true);
+            });
+        // TODO add error handling
+    }
+
+    function handleSubmit() {
+        handleCreate();
+        // TODO add redirect etc
+    }
+
+    const getSubmitButtonText = () => {
+        if (error) {
+            return "Retry Create";
+        }
+        return "Create";
+    };
+
+    function handleFormChange(changedField) {
+        const newState = Object.assign({}, state, changedField);
+        setError(false);
+        setState(newState);
+    }
+
+    return (
+        <Modal
+            title={title}
+            applyButtonText={getSubmitButtonText()}
+            disableApply={!state.name || !state.type}
+            onApply={handleSubmit}
+            onDismiss={() => setDisplayModal(false)}
+        >
+            {error && (
+                <Notification type="error">
+                    An error occurred and the page could not be created. Please try again
+                    or contact support if the issue persists.
+                </Notification>
+            )}
+            <CreatePageForm name={state.name} onChange={handleFormChange} />
+        </Modal>
+    );
+}
+
+export { CreatePageModal };
